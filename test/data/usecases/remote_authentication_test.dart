e76a2c371c70fc4969df1bff40a34632a80e4f71
tiago <tiago.moreira@timebrisa.com.br>
2022-08-25 11:29:08 -0300

    feat: :sparkles: Ensure RemoteAuthentication trows UnexpectedError on 200 with invalid data

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -105,4 +105,18 @@ void main() {
 
     expect(account.token, accessToken);
   });
+
+  test(
+      'Should throw UnexpetedError if HttpClient returns 200 with invalid data',
+      () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenAnswer((_) async => {'invalid_key': 'invalid_value'});
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
