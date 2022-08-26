9f5ff31472f28420deb8762e130c2239bc0079ac
tiago <tiago.moreira@timebrisa.com.br>
2022-08-26 08:53:57 -0300

    feat: :sparkles: Ensure HttpAdapter calls post with correct Headers

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -15,7 +15,11 @@ class HttpAdapter {
     @required String method,
     // Map body,
   }) async {
-    await client.post(url);
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json',
+    };
+    await client.post(url, headers: headers);
   }
 }
 
@@ -30,7 +34,13 @@ void main() {
 
       await sut.request(url: url, method: 'post');
 
-      verify(client.post(url));
+      verify(client.post(
+        url,
+        headers: {
+          'content-type': 'application/json',
+          'accept': 'application/json',
+        },
+      ));
     });
   });
 }
