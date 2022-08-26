2d1a99275f84190c2b6de5f8326308bd772d0fa0
tiago <tiago.moreira@timebrisa.com.br>
2022-08-26 09:30:54 -0300

    feat: :sparkles: Ensure HttpAdapter calls post without body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -20,7 +20,12 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client.post(url, headers: headers, body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(
+      url,
+      headers: headers,
+      body: jsonBody,
+    );
   }
 }
 
@@ -39,8 +44,11 @@ void main() {
 
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut
-          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+      await sut.request(
+        url: url,
+        method: 'post',
+        body: {'any_key': 'any_value'},
+      );
 
       verify(client.post(url,
           headers: {
@@ -48,6 +56,18 @@ void main() {
             'accept': 'application/json',
           },
           body: '{"any_key":"any_value"}'));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(
+        url: url,
+        method: 'post',
+      );
+
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
+      ));
     });
   });
 }
