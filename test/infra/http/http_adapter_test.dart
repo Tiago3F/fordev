e429be82749757699a370b1bc2d2936b18bb4e6c
tiago <tiago.moreira@timebrisa.com.br>
2022-08-26 13:08:23 -0300

    feat: :sparkles:

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -83,6 +83,15 @@ void main() {
       expect(response, null);
     });
 
+    test('Should return BadRequestError if post returns 400 with body empty',
+        () async {
+      mockResponse(400, body: '');
+
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
     test('Should return BadRequestError if post returns 400', () async {
       mockResponse(400);
 
